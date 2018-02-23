6476b2dd32e2381597d568b62874ffc7cfc4abfe
Ania <ania.makes@gmail.com>
2018-02-23 16:33:30 +0000

    added second test for charCoder function

--- tests/charCoder.test.js
+++ tests/charCoder.test.js
@@ -2,7 +2,10 @@ const charCoder = require('../lib/charCoder');
 
 describe('charCoder', function(){
   test('String transformed into a number string', function(){
-      const expected1 = "6511010597"
+      const expected1 = "6511010597";
       expect(charCoder("Ania")).toEqual(expected1);
+
+      const expected2 = "73321081111181013212111111733";
+      expect(charCoder("I love you!")).toEqual(expected2);
   });
 });
