4a532a265f649fd3a298a6d4ff59b0ca018b13ad
Ania <ania.makes@gmail.com>
2018-02-27 20:56:48 +0000

    streams without issues

--- server.js
+++ server.js
@@ -1,8 +1,46 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-
 const bookSearch = require('./lib/google-book-search');
+
+const Twit = require('twit');
+const T = new Twit({
+  consumer_key:         process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.CONSUMER_SECRET,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms:           60*1000,
+})
+
+let stream = T.stream('statuses/filter', {track: 'cook'})
+
+stream.on('tweet', function (tweet){
+  const tweetText = tweet.text;
+  const username = tweet.user.screen_name;
+  console.log(tweetText, username);
+  console.log(generateReplyAndTweetIt(tweetText));
+  console.log("------");
+})
+
+function generateReplyAndTweetIt (queryString){
+  bookSearch(queryString, function(error, results, msg) {
+    if (msg) {
+      console.log(msg);
+    }
+
+  // bookSearch generates an array. If the array is empty, response is a string stating so. If it's not empty, book gets processed and spits out a book.
+  let book;
+
+  if (results && results.length > 0){
+    console.log("1: ", results);
+    book = processBookSearch(results);
+  };
+
+  if (book != undefined){
+    console.log("2: ",book);
+  }
+});
+}
 
 
 // const fetch = require("node-fetch");
@@ -25,30 +63,36 @@ app.post("/process-query", function(req, res){
       console.log(msg);
     }
 
-    const resultsLength = results.length;
-    let randomNumber = Math.floor(Math.random() * resultsLength);
+    let book = processBookSearch(results);
 
-    const randomBook = results[randomNumber];
-    console.log(randomBook);
-
-    let outputBookInfo = {};
-
-    outputBookInfo.title = randomBook.title;
-    outputBookInfo.coverImage = randomBook.thumbnail;
-
-    if (randomBook.authors){
-      outputBookInfo.author = randomBook.authors[0];
-    } else {
-      outputBookInfo.author = "An Unknown Author";
-    }
-
-    console.log("BOOKS IN SERVER : ", outputBookInfo);
-
-    res.json(outputBookInfo);
-
+    res.json(book);
   });
 
 });
+
+function processBookSearch(results){
+  const resultsLength = results.length;
+  let randomNumber = Math.floor(Math.random() * resultsLength);
+
+  const randomBook = results[randomNumber];
+  console.log(randomBook);
+
+  let outputBookInfo = {};
+
+  outputBookInfo.title = randomBook.title;
+  outputBookInfo.coverImage = randomBook.thumbnail;
+
+  if (randomBook.authors){
+    outputBookInfo.author = randomBook.authors[0];
+  } else {
+    outputBookInfo.author = "An Unknown Author";
+  }
+
+  console.log("BOOKS IN SERVER : ", outputBookInfo);
+
+  return outputBookInfo;
+}
+
 
 app.listen(8080, function(){
   console.log("listening on port 8080");
